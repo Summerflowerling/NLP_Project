836658b17e4f9d0af4a7f1f1096cf4116f0528b4
yuling <yulinglin@protonmail.com>
2021-03-21 21:16:41 +0800

    Fixed the syntax of expect() in test

--- projects/evaluate-news-nlp/src/_test_/testContentCheck.js
+++ projects/evaluate-news-nlp/src/_test_/testContentCheck.js
@@ -1,9 +1,9 @@
-import {checkForContent } from "../src/client/js/contentChecker"
+import {checkForContent } from "./src/client/js/contentChecker"
 
 describe("Testing if user type in any text", () => {
     
     test("Testing the if textArea content is empty string ", () => {
         let input = "";
             
-           expect(checkForContent(input).toBeTruthy());
+           expect(checkForContent(input)).toBeTruthy();
 })});
--- projects/evaluate-news-nlp/src/_test_/testFormHandler.spec.js
+++ projects/evaluate-news-nlp/src/_test_/testFormHandler.spec.js
@@ -1,10 +1,10 @@
-import { updateUi } from "../client/js/formHandler";
-import { handleSubmit } from "..src/client/js/formHandler"
+import { updateUi } from "./client/js/formHandler";
+import { handleSubmit } from "./client/js/formHandler"
 
 
 describe("Testing the submit functionality", () => {
     
     test("Testing the handleSubmit() function", () => {
             
-           expect(handleSubmit().toBeDefined());
+           expect(handleSubmit()).toBeDefined();
 })});
