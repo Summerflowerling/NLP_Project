925fffba759185413a010ed5c17384ce21842ed2
yuling <yulinglin@protonmail.com>
2021-03-22 08:56:15 +0800

    Changed the files in test to spec.js file Changed the import path

---
+++ projects/evaluate-news-nlp/src/_test_/testContentCheck.spec.js
@@ -0,0 +1,9 @@
+import {checkForContent } from "../client/js/contentCheck"
+
+describe("Testing if user type in any text", () => {
+    
+    test("Testing the if textArea content is empty string ", () => {
+        let input = "";
+            
+           expect(checkForContent).toBeTruthy();
+})});
