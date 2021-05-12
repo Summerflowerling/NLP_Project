b6009b26dbec3b8456cf3bd02e12c56e2ff6404e
yuling <yulinglin@protonmail.com>
2021-05-12 11:20:50 +0800

    refractor: Display different title and content in result section  according to the data returned
    Works correctly.
    TODO: Add explaination for if condition in formHandler.js

--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -6,21 +6,40 @@ function updateUi (data){
     const irony = document.getElementById("irony")
     const resultList = document.getElementById("result-list")
     const sectionResult = document.getElementById("section-result")
+    
+
+    
+
+    
 
     if(data.status.code != "201"){
+        if (sectionResult.firstElementChild.classList.contains("result-no-resource")){
+            sectionResult.removeChild(sectionResult.firstChild);
+        }
+        
+        resultList.style.display ="block"
         agreement.innerHTML = `<span class="result-list-title">Agreement</span>: ${data.agreement.toLowerCase()}`
         subjectivity.innerHTML = `<span class="result-list-title">Subjectivity</span>: ${data.subjectivity.toLowerCase()}`
         confidence.innerHTML = `<span class="result-list-title">Confidence</span>: ${data.confidence.toLowerCase()}  % `
         irony.innerHTML = `<span class="result-list-title">Irony</span>: ${data.irony.toLowerCase()}`
+        
 
         if(!sectionResult.firstElementChild.classList.contains("result-list-section-title")){
-            resultList.insertAdjacentHTML("beforebegin", `<h3 class="result-list-section-title">This is your results</h3>`)    
+            resultList.insertAdjacentHTML("beforebegin", `<h3 id="result-list-title" class="result-list-section-title">This is your results</h3>`) 
+            
             } else {
-            return
+                document.getElementById("result-list-title").style.display="block"   
             }
 
     }else {
-        resultList.insertAdjacentHTML("beforebegin", `<h1 class="result_no_resource">${"Sorry there is no result for your content"}</h3>`)    
+        if(!sectionResult.firstElementChild.classList.contains("result-no-resource")){
+            sectionResult.insertAdjacentHTML("afterbegin", `<h1 id="no-result" class="result-no-resource">${"Sorry there is no result for your content"}</h3>`)    
+            resultList.style.display ="none"
+            document.getElementById("result-list-title").style.display="none"
+        } else{
+            return
+        }
+        
 
     }
     
@@ -33,6 +52,7 @@ function updateUi (data){
 
 function handleSubmit(event) {
     event.preventDefault()
+    
     // check what text was put into the form field
     let formText = document.getElementById('text').value
     Client.checkForContent(formText)
